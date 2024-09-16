afd196c526ec317e596536e6a2615d5708b6c979
Shigi <emmanuelgichigo@gmail.com>
2024-09-16 09:25:23 +0300

    Inserted records to gender table

--- db/db.sql
+++ db/db.sql
@@ -4,3 +4,5 @@ CREATE TABLE gender (
     genderId INT PRIMARY KEY AUTO_INCREMENT,
     gender VARCHAR(20) NOT NULL
 );
+
+INSERT INTO gender (genderId, gender) VALUES (1, 'Male'), (2, 'Female'), (3, 'Other');
