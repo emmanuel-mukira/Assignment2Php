0798bc68ed63fa7fa31e8958690d99ab5fc71eef
Shigi <emmanuelgichigo@gmail.com>
2024-09-16 09:27:13 +0300

    Created users table

--- db/db.sql
+++ db/db.sql
@@ -13,3 +13,15 @@ CREATE TABLE roles (
 );
 
 INSERT INTO roles (roleId, role) VALUES (1, 'Admin'), (2, 'User'), (3, 'Guest');
+
+CREATE TABLE users (
+    userId INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    genderId INT,
+    roleId INT,
+    CONSTRAINT fk_genderId FOREIGN KEY (genderId) REFERENCES gender(genderId),
+    CONSTRAINT fk_roleId FOREIGN KEY (roleId) REFERENCES roles(roleId)
+);
