7c61e9b1b6c5b8d2e1dd98b49879a6f561b9ab81
Shigi <emmanuelgichigo@gmail.com>
2024-09-19 13:48:13 +0300

    load

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-require 'vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-// Now you can access your .env variables
-$customVar = getenv('MY_CUSTOM_VARIABLE');
-
     require_once "load.php";
+    
     $ObjLayouts->heading();
     $ObjMenus->main_menu();
     $ObjLayouts->banner();
