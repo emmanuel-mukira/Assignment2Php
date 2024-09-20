7c5c75f260533e7b4133fd94be431693ad36bc9d
Shigi <emmanuelgichigo@gmail.com>
2024-09-20 20:05:20 +0300

    About us

---
+++ aboutus.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "load.php";
+    $ObjLayouts->heading();
+    $ObjMenus->main_menu();
+    $ObjLayouts->banner();
+    $ObjContents->about_content();
+    $ObjContents->sidebar();
+    $ObjLayouts->footer();
