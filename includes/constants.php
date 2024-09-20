ac20d3361b978d68f5dcd4370d3c69f09c57ecee
Shigi <emmanuelgichigo@gmail.com>
2024-09-20 09:24:24 +0300

    fixing errors

--- includes/constants.php
+++ includes/constants.php
@@ -1,18 +1,12 @@
 <?php
 
-// Include the autoloader (assuming you're using Composer)
-require 'vendor/autoload.php';
 
-// Load the .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 // Define constants using environment variables
-define('DBTYPE', getenv('DB_TYPE'));
-define('HOSTNAME', getenv('DB_HOSTNAME'));
-define('DBPORT', getenv('DB_PORT'));
-define('HOSTUSER', getenv('DB_USERNAME'));
-define('HOSTPASS', getenv('DB_PASSWORD'));
-define('DBNAME', getenv('DB_NAME'));
+define('DBTYPE','PDO' );
+define('HOSTNAME', 'localhost');
+define('DBPORT', '3306');
+define('HOSTUSER', 'root');
+define('HOSTPASS', 'shigi460');
+define('DBNAME', 'users');
 
-// Now, these constants will be populated from your .env file
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-    require_once "load.php";
-    
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once "load.php";
+
     $ObjLayouts->heading();
     $ObjMenus->main_menu();
     $ObjLayouts->banner();
