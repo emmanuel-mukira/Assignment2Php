c3522d2d085b0d97a7f6c111bb0778900254a252
Shigi <emmanuelgichigo@gmail.com>
2024-09-20 09:29:45 +0300

    Layouts

--- includes/dbConnection.php
+++ includes/dbConnection.php
@@ -1,7 +1,8 @@
 <?php
 require 'constants.php';
+
 try{
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$HOSTNAME;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
